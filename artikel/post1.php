812ef5fe6db819ab5dfa849e04b8eefbbd0cda74
tarmizi <tarmizilsm83@gmail.com>
2022-05-30 07:51:20 +0700

    website blog

---
+++ artikel/post1.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>NR16 Multimedia</title>
+  </head>
+  <body>
+<!--header-->
+<main>
+ 
+  <div class="b-example-divider"></div>
+
+  <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="../index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+      <img src="../assets/img/logo.png" class="img-fluid" alt="..." style="max-width: 70px; max-height : 70px">
+      </a>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="../index.php" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+        <li><a href="../index.php" class="nav-link px-2 link-dark">Contact</a></li>
+      </ul>
+      <div class="col-md-3 text-end">
+      </div>
+    </header>
+  </div>
+
+</main>
+
+  <img src="../assets/img/welcome.png" class="img-fluid" alt="..." style="align-items: center; width: 70%; height: 70%; padding-left : 30% ; ">
+
+
+  <div class="card" style="padding-left : 7%; padding-right : 7% ;padding-top : 2%; border : none">
+  <div class="card-body">
+
+<p>"Halo, perkenalkan nama saya Nasrullah Rinaldi, atau biasa disapa Nasrul atau Nas, saya merupakan seorang Mahasiswa dari Kota Lhokseumawe yang memiliki ketertarikan di dunia Multimedia,Desain Grafis,Editing,dan Teknologi. </p>
+<Singkatnya,>Blog ini merupakan blog pertama saya dan saya kelola sendiri. blog ini saya berinama NR16 Multimedia.Singkatnya, NR16 Multimedia. merupakan situs blog yang membahas topik mengenai Dunia Multimedia seperti  Desain Grafis, Mengedit Vide atau Foto, dan pembahasan menarik lainnya yang masih berhubungan dengan Multimedia.</p>
+<p>Situs ini sudah ada pada tahun 2020 tetapi baru diaktifkan pada tahun 2022, dengan tujuan untuk membagikan tulisan saya kepada pengguna internet, khususnya kepada mereka yang ingin belajar mengenai Multimedia.</p>
+<p>Saat ini Panduanblog.id menyediakan tulisan yang berhubungan dengan topik yang sudah saya sebutkan diatas, namun untuk kedepannya, saya akan berusaha untuk menghadirkan konten yang lebih berkualitas dan bermanfaat untuk semua orang.</p>
+<p>Demikian perkenalan singkat mengenai saya sebagai pemilik dan penulis situs NR16 Multimedia. Apabila ada hal yang ingin ditanyakan, khususnya mengenai bisnis, silahkan hubungi saya, Terima kasih."</p>
+  </div>
+</div>
+
+<!--footer--->
+<div class="b-example-divider"></div>
+
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="../index.php" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      <img src="../assets/img/logo.png" class="img-fluid" alt="..." style="width: 40px; height : 44px">
+      </a>
+      <span class="text-muted">&copy; 2021 NR16 Multimedia</span>
+    </div>
+  </footer>
+</div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
